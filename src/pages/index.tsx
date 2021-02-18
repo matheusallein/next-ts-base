907710cd5c4b8555945096762a1f1352177b618f
Matheus Allein <mtsallein@gmail.com>
2021-02-18 19:14:56 -0300

    Initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import { Container, Typography } from '@material-ui/core'
+import DefaultSVG from '../assets/default.svg'
+import PlaceholderJPG from '../assets/placeholder.jpg'
 
 const Home: React.FC = () => {
   return (
     <Container>
       <Head>
-        <title>User C.R.U.D.</title>
+        <title>Next.js TypeScript Base</title>
       </Head>
       <main>
         <Typography color="secondary">Hello, World!</Typography>
+        <DefaultSVG />
+        <Image
+          src={PlaceholderJPG}
+          alt="Placeholder"
+          width={350}
+          height={150}
+        />
       </main>
     </Container>
   )
